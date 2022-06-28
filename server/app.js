913c36c190cb03066e57bac772821888107b6dfe
Yonatan Levin <parahall@gmail.com>
2022-06-28 20:07:47 +0300

    Push to Heroku:

--- server/app.js
+++ server/app.js
@@ -1,9 +1,12 @@
 const express = require("express");
+const path = require('path');
 const movieRouter = require('./router/moviesRouter');
 const PORT = process.env.PORT || 3001;
 
 const app = express();
 app.use(express.json())
+
+app.use(express.static(path.resolve(__dirname, '../client/build')));
 
 app.get('/', (req, res, next) => {
     res.status(200).json({
